2a0c9766837f6b9af8233f02ffd689aabbd2101e
hussin-mohamed <146856442+hussin-mohamed@users.noreply.github.com>
2023-10-18 22:28:13 +0300

    Update extend.v

--- extend.v
+++ extend.v
@@ -17,7 +17,7 @@ module extend (
             2'b01:ImmExt={{20{Instr[31]}},Instr[31:25],Instr[11:7]};
             /*B-Type*/
             2'b10:ImmExt={{20{Instr[31]}},Instr[7],Instr[30:25],Instr[11:8],1'b0}; 
-            2'b11:ImmExt={{12{Instr[31]}}, Instr[19:12], Instr[20], Instr[30:21], 0}; 
+            2'b11:ImmExt={{12{Instr[31]}}, Instr[19:12], Instr[20], Instr[30:21], 1'b0}; 
             default: ImmExt=0;
         endcase
     end
