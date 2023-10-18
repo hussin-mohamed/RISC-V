1322ab38ef5965439376989c3b429ec42137f45e
hussin-mohamed <146856442+hussin-mohamed@users.noreply.github.com>
2023-10-18 22:25:21 +0300

    Update Register_file.v

--- Register_file.v
+++ Register_file.v
@@ -2,8 +2,7 @@
 module Register_file(input signed [31:0]  data_received,input clk,write,input[4:0] src_add1,src_add2,write_add,output  reg signed [31:0] datasent1,datasent2   );
 reg [31:0] ram[2**5-1:0];
 initial  begin
-    ram[5]=6;
-    ram[9]=4;
+    ram[0]=0;
 end
 always@(negedge clk)
 begin
