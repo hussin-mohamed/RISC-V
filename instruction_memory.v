a0031aa36fa38a51e59e58030866cd0899f3d52a
Hussien Mohamed <hussein.10.7.2003@gmail.com>
2024-08-11 09:14:54 +0300

    Update instruction_memory.v

--- instruction_memory.v
+++ instruction_memory.v
@@ -2,8 +2,12 @@
 module instruction_memory 
 #(parameter DATA_WIDTH=8, parameter ADDR_WIDTH=32)
 (
+	// input [(ADDR_WIDTH-1):0] addr,
+	// output reg [((DATA_WIDTH*4)-1):0] q
+	input [(DATA_WIDTH-1):0] data,
 	input [(ADDR_WIDTH-1):0] addr,
-	output reg [((DATA_WIDTH*4)-1):0] q
+	input we, clk,
+	output reg signed  [(32-1):0] q
 );
 
 	// Declare the RAM variable
@@ -14,10 +18,15 @@ module instruction_memory
     end
 	// Variable to hold the registered read address
 	reg [ADDR_WIDTH-1:0] addr_reg;
-
+	always @ (negedge clk)
+	begin
+		// Write
+		if (we)
+			rom[addr] <= data;
+	end
 	always @ (*)
 	begin
-		q<={rom[addr],rom[addr+1],rom[addr+2],rom[addr+3]};
+		q={rom[addr],rom[addr+1],rom[addr+2],rom[addr+3]};
 	end
 
 endmodule
